541b40633d60e08e47302fc23d8acba40a27bbe5
harshil3030 <savaliyaharshil3030@gmail.com>
2024-09-14 07:44:06 +0530

    food-dep panel updated

--- app/src/main/java/com/example/hostelhomes/Foodstaffhomeactivity.java
+++ app/src/main/java/com/example/hostelhomes/Foodstaffhomeactivity.java
@@ -15,11 +15,7 @@ public class Foodstaffhomeactivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_foodstaffhomeactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
 
     }
 }
