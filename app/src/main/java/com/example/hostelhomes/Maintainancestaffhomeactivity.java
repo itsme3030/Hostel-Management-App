9e17321597a972cbeaadbe58368561faafe48f1f
harshil3030 <savaliyaharshil3030@gmail.com>
2024-09-14 06:43:01 +0530

    adminpanel updated

--- app/src/main/java/com/example/hostelhomes/Maintainancestaffhomeactivity.java
+++ app/src/main/java/com/example/hostelhomes/Maintainancestaffhomeactivity.java
@@ -15,10 +15,8 @@ public class Maintainancestaffhomeactivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_maintainancestaffhomeactivity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+
     }
 }
